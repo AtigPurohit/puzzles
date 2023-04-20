ddf1e8cdcf144ca9397b467578316891042f7f3a
Farhan Syed <syedfarhanali1010@gmail.com>
2023-04-20 18:55:43 +0530

    Adding the log in page

---
+++ login_page/app.js
@@ -0,0 +1,13 @@
+const loginBtn = document.getElementById('login-btn');
+const signupBtn = document.getElementById('signup-btn');
+
+
+signupBtn.addEventListener('click', () => {
+    window.location.href = 'signup.html';
+});
+
+
+loginBtn.addEventListener('click', () => {
+    window.location.href = 'login.html';
+  });
+
